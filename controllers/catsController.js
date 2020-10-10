4d08c5ca1229f2b1a9712884eb7b7b2fc98cba4e
David <Jaffe>
2020-10-10 15:44:53 -0700

    added burgers

--- controllers/catsController.js
+++ controllers/catsController.js
@@ -9,21 +9,21 @@ var cat = require("../models/cat.js");
 router.get("/", function(req, res) {
   cat.all(function(data) {
     var hbsObject = {
-      cats: data
+      burgers: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
 
-router.post("/api/cats", function(req, res) {
+router.post("/api/burgers", function(req, res) {
   cat.create(["name", "sleepy"], [req.body.name, req.body.sleepy], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
-router.put("/api/cats/:id", function(req, res) {
+router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
 USE burgers_db;
 
-INSERT INTO cats (name) VALUES ('Bacon Cheese');
-INSERT INTO cats (name) VALUES ('Bacon Blue');
-INSERT INTO cats (name, sleepy) VALUES ('Salmon Burger', true);
-INSERT INTO cats (name, sleepy) VALUES ('Miyako Burger', true);
-INSERT INTO cats (name, sleepy) VALUES ('Pubhouse Burger', true);
-INSERT INTO cats (name) VALUES ('Wilbur Jumbo Deluxe');
+INSERT INTO burgers (name) VALUES ('Bacon Cheese');
+INSERT INTO burgers (name) VALUES ('Bacon Blue');
+INSERT INTO burgers (name, sleepy) VALUES ('Salmon Burger', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Miyako Burger', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Pubhouse Burger', true);
+INSERT INTO burgers (name) VALUES ('Wilbur Jumbo Deluxe');
