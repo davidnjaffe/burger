099379bd192faf64ee44df1ae37b9a82ef47f283
David <Jaffe>
2020-10-10 13:18:24 -0700

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO cats (name) VALUES ('Bacon Cheese');
+INSERT INTO cats (name) VALUES ('Bacon Blue');
+INSERT INTO cats (name, sleepy) VALUES ('Salmon Burger', true);
+INSERT INTO cats (name, sleepy) VALUES ('Miyako Burger', true);
+INSERT INTO cats (name, sleepy) VALUES ('Pubhouse Burger', true);
+INSERT INTO cats (name) VALUES ('Wilbur Jumbo Deluxe');
